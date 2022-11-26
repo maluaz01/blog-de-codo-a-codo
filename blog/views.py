85cfcba80dff9e79b21f55dfb1fde25ceabd8ae4
florencia rodriguez <florenciarroux@gmail.com>
2022-11-26 12:41:56 -0300

    Subo primer version del front

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,75 @@ from django.template import loader
 from blog.forms import ContenidoForm
 from django.contrib import messages
 
+
+def inicio(request):
+    return render(request, 'blog/index.html', {})
+
+def ver_contenido(request):
+    lista_posts = [
+        {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+        {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+         {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+         {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+         {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+          {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+         {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+         {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+        {
+            'titulo':'Titulo',
+            'autor':'Autor',
+            'categoria':'Categoria',
+            'contenido':'Contenido'
+        },
+        
+    ]
+
+    return render(request, 'blog/ver_contenido.html', {'contenidos':lista_posts})
+
+
+def perfil(request):
+    return render(request, 'blog/perfil.html', {})
+
 def lista_de_post(request):
     posts =Post.objects.filter(fecha_de_publicacion__lte=timezone.now()).order_by('fecha_de_publicacion')
     return render(request, 'blog/lista_de_post.html', {'posts': posts})
